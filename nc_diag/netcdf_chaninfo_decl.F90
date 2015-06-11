b3f86e9e8e0bbbbbcaa53b52fcf208ead598c752
Albert Huang <alberthdev@users.noreply.github.com>
2015-06-11 10:51:47 -0400

    Added type variable descriptions + general sub interface

--- nc_diag/netcdf_chaninfo_decl.F90
+++ nc_diag/netcdf_chaninfo_decl.F90
@@ -24,12 +24,21 @@
             ! also the multiplier factor for allocation (2^x)
             integer(i_byte)                               :: alloc_multi
             
+            ! Name array for each variable
             character(len=100),  dimension(:),allocatable :: names
+            ! Type constants array for each variable
             integer(i_byte),     dimension(:),allocatable :: types
+            ! Variable array usage count (which, for each element,
+            ! should be <= nchans)
+            integer(i_byte),     dimension(:),allocatable :: acount_v
+            ! Total variables stored
             integer(i_long)                               :: total
+            ! Array size for each type array
             integer(i_long),     dimension(6)             :: asize
+            ! Array count for each type array
             integer(i_long),     dimension(6)             :: acount
             
+            ! Storage arrays for specific types
             integer(i_byte),     dimension(:),allocatable :: ci_byte
             integer(i_short),    dimension(:),allocatable :: ci_short
             integer(i_long),     dimension(:),allocatable :: ci_long
@@ -38,4 +47,13 @@
             character(len=1000), dimension(:),allocatable :: ci_string
         end type diag_chaninfo
         
-        type(diag_header), allocatable :: diag_chaninfo_store
+        type(diag_chaninfo), allocatable :: diag_chaninfo_store
+        
+        interface nc_diag_chaninfo
+            module procedure nc_diag_chaninfo_byte, &
+                nc_diag_chaninfo_short, nc_diag_chaninfo_long, &
+                nc_diag_chaninfo_rsingle, nc_diag_chaninfo_rdouble, &
+                nc_diag_chaninfo_string, nc_diag_chaninfo_byte_v, &
+                nc_diag_chaninfo_short_v, nc_diag_chaninfo_long_v, &
+                nc_diag_chaninfo_rsingle_v, nc_diag_chaninfo_rdouble_v
+        end interface nc_diag_chaninfo
