ed36c0b19a7127d3fc81a66bbd6c45cf3099d80b
Albert Huang <alberthdev@users.noreply.github.com>
2015-06-17 10:42:33 -0400

    Overhauled header code to just be a simple wrapper for NetCDF4
    Removed all caching code for the header part, since it's just modifying
    the NetCDF4 global attributes, which are meant for overwriting. Since
    it's much simpler (no data storage needed, no appending, etc.), the
    caching code was scrapped in favor of the header portion becoming a
    thin wrapper for the NetCDF4 nf90_put_att function. This allows the
    header writing to be MUCH faster, due to removing unneeded
    functionality.

--- nc_diag/netcdf_layer.F90
+++ nc_diag/netcdf_layer.F90
@@ -9,7 +9,7 @@ module netcdf_layer
     real(r_single),dimension(:),allocatable :: bifix
     !real(r_single),dimension(:),allocatable    :: bifix
     
-    ! diag_header struct constants
+    ! NetCDF4 type struct constants
     integer(i_byte), parameter              :: NLAYER_BYTE   = 1
     integer(i_byte), parameter              :: NLAYER_SHORT  = 2
     integer(i_byte), parameter              :: NLAYER_LONG   = 3
@@ -33,7 +33,6 @@ module netcdf_layer
     
     contains
 #include "netcdf_realloc_imp.f90"
-#include "netcdf_hresize.F90"
 #include "netcdf_ciresize.F90"
 #include "netcdf_lheader_imp.F90"
 #include "netcdf_chaninfo_imp.F90"
@@ -60,10 +59,9 @@ module netcdf_layer
             ! here.
             if (.NOT. init_done) then
                 call check( nf90_create(filename, OR(NF90_NETCDF4, NF90_CLOBBER), ncid) )
-                if (allocated(diag_header_store)) then
-                    call error("BUG! diag_header_store is allocated, but init_done is set!")
+                if (allocated(diag_chaninfo_store)) then
+                    call error("BUG! diag_chaninfo_store is allocated, but init_done is set!")
                 end if
-                allocate(diag_header_store)
                 allocate(diag_chaninfo_store)
                 init_done = .TRUE.
             else
@@ -124,7 +122,6 @@ module netcdf_layer
         !end subroutine nc_diag_data
         
         subroutine nc_diag_write
-            call nc_diag_header_write
             call nc_diag_chaninfo_write_def
             
             ! Lock definition writing!
