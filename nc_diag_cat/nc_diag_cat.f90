58dce298b13c155738c894950749200a353004ff
Albert Huang <alberthdev@users.noreply.github.com>
2015-07-28 17:46:10 -0400

    Added nc_diag_cat step benchmarking

--- nc_diag_cat/nc_diag_cat.f90
+++ nc_diag_cat/nc_diag_cat.f90
@@ -15,7 +15,11 @@ program nc_diag_cat
     integer, parameter                 :: NC_DIAG_CAT_GZIP_COMPRESS = 6
     integer, parameter                 :: NC_DIAG_CAT_CHUNK_SIZE = 16384
     
+    character(len=300)                 :: info_str
     call nc_diag_cat_process_args
+    
+    
+    real :: start_time, stop_time
     
 #ifndef NO_NETCDF
     write (*,"(A, A, A)") 'Initializing netcdf layer library, version ', trim(nf90_inq_libvers()), '...'
@@ -35,16 +39,37 @@ program nc_diag_cat
     !   3) Read each file again, this time concatenating everything
     !      as we go.
     
+    call cpu_time(start_time)
     call nc_diag_cat_metadata_pass
+    call cpu_time(stop_time)
+    
+    write (info_str, "(A, F, A)") "Metadata read took ", stop_time - start_time, " seconds!"
+    call info(trim(info_str))
     call nc_diag_cat_metadata_define
     
 #ifdef DEBUG
     print *, "MAIN: trigger data pass!"
 #endif
     
+    call cpu_time(start_time)
     call nc_diag_cat_metadata_alloc
+    call cpu_time(stop_time)
+    
+    write (info_str, "(A, F, A)") "Data preallocation took ", stop_time - start_time, " seconds!"
+    call info(trim(info_str))
+    call cpu_time(start_time)
     call nc_diag_cat_data_pass
+    call cpu_time(stop_time)
+    
+    write (info_str, "(A, F, A)") "Data read took ", stop_time - start_time, " seconds!"
+    call info(trim(info_str))
+    
+    call cpu_time(start_time)
     call nc_diag_cat_data_commit
+    call cpu_time(stop_time)
+    
+    write (info_str, "(A, F, A)") "Data commit took ", stop_time - start_time, " seconds!"
+    call info(trim(info_str))
         
 #ifdef DEBUG
     print *, "ALL DONE!"
@@ -52,8 +77,12 @@ program nc_diag_cat
     
     call info("All data queued, letting NetCDF take over (and actually write)!")
     
+    call cpu_time(start_time)
     call check(nf90_close(ncid_output))
+    call cpu_time(stop_time)
     
+    write (info_str, "(A, F, A)") "Final data write took ", stop_time - start_time, " seconds!"
+    call info(trim(info_str))
     call info("All done!")
     
     contains
